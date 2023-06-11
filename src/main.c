53441c497d84b43717668e4de0be1691f004c781
Gabriele Invernizzi <gabrieleinvernizzi4@gmail.com>
2023-06-11 13:36:16 +0200

    Created term_renderer.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include "term_renderer.h"
 
 #include <stdlib.h>
+#include <time.h>
+
+#define WIDTH (30)
+#define HEIGHT (20)
+
+static float* state = NULL;
+
+float rand_float(void) {
+    return (float)rand() / (float)RAND_MAX;
+}
+
+void state_init(void) { 
+    srand(time(0));
+
+    float *buffer = malloc(WIDTH * HEIGHT * sizeof(float));
+    for (unsigned int i = 0; i < HEIGHT; i++) {
+        for (unsigned int j = 0; j < WIDTH; j++)
+            buffer[j + WIDTH*i] = rand_float();
+    }
+}
+
+void state_deinit(void) {
+    if (state) free(state);
+}
+
+
 
 int main(const int argc, const char** argv) {
-    trenderer_init(0, 0);
-    trenderer_render(NULL);    
+    state_init();
+
+    trenderer_init(WIDTH, HEIGHT);
+    trenderer_render(state);    
+    trenderer_deinit();
+    state_deinit();
 }
--- src/term_renderer.c
+++ src/term_renderer.c
@@ -1,5 +1,6 @@
 #include "term_renderer.h"
 
+#include <stdlib.h>
 #include <stdio.h>
 
 // Define brightness levels.
@@ -10,16 +11,43 @@ static const unsigned char N_LEVELS = sizeof(LEVELS) / sizeof(char) - 1;
 typedef struct {
     unsigned int width;
     unsigned int height;
+    unsigned int buf_len;
+    char *buffer;
 } TRenderer;
 
 static TRenderer trenderer = { 0 };
 
+static inline void trenderer_set_buf_at(unsigned int i, unsigned int j, char val) {
+    trenderer.buffer[j + i * (trenderer.width + 1)] = val;
+}
+
+static inline char trenderer_get_buf_at(unsigned int i, unsigned int j) {
+    return trenderer.buffer[j + i * (trenderer.width + 1)];
+}
+
 void trenderer_init(unsigned int width, unsigned int height) {
     trenderer.width = width;
     trenderer.height = height;
+    trenderer.buf_len = (width + 1) * height;
+    trenderer.buffer = malloc(trenderer.buf_len * sizeof(char));
+    for (unsigned int i = 0; i < height; i++)
+        trenderer_set_buf_at(i, width, '\n');
 }
 
+void trenderer_deinit(void) {
+    if (trenderer.buffer) free(trenderer.buffer);
 
-void trenderer_render(const float** frame) {
-    printf("Helloo.\n");
+    trenderer.buf_len = 0;
+    trenderer.width = 0;
+    trenderer.height = 0;
+}
+
+void trenderer_render(float* frame) {
+    for (unsigned int i = 0; i < trenderer.height; i++) {
+        for (unsigned int j = 0; j < trenderer.width; j++) {
+            unsigned int level = (unsigned int)((frame[j + trenderer.width*i] * N_LEVELS));
+            trenderer_set_buf_at(i, j, LEVELS[level < N_LEVELS ? level : (N_LEVELS - (unsigned int)1)]);
+        }
+    }
+    fwrite(trenderer.buffer, sizeof(char), trenderer.buf_len, stdout);
 }
