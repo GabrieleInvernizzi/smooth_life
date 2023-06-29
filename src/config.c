4a50361aaa8ac731fb3c611c006400148f889f49
Gabriele Invernizzi <gabrieleinvernizzi4@gmail.com>
2023-06-29 18:12:18 +0200

    Added more configuration options and fixed forgotten toml_free.

--- src/config.c
+++ src/config.c
@@ -26,9 +26,10 @@ static inline char* str_to_lower(char* s) {
         return -1;\
     }
 
-static int parse_conf(SMConfig* conf, FILE* f) {
+static int parse_conf(Config* config, FILE* f) {
     toml_datum_t current_val;
     char errbuf[PARSE_CONF_ERRBUF_SIZE];
+    SMConfig* conf = &config->sm_conf;
 
     const toml_table_t* table = toml_parse_file(f, errbuf, sizeof(errbuf));
     if (!conf) {
@@ -36,8 +37,24 @@ static int parse_conf(SMConfig* conf, FILE* f) {
         return -1;
     }
 
-    if (toml_key_exists(table, "execution_policy")) {
-        current_val = toml_string_in(table, "execution_policy");
+    const toml_table_t* rendering_conf = toml_table_in(table, "rendering");
+    if (rendering_conf) {
+        if (toml_key_exists(rendering_conf, "max_fps")) {
+            current_val = toml_int_in(rendering_conf, "max_fps");
+            PARSE_CONF_CHECK(current_val, "max_fps")
+            if (current_val.u.i > 0)
+                config->max_fps = current_val.u.i;
+        }
+    }
+
+    const toml_table_t* sm_conf = toml_table_in(table, "simulation");
+    if (!sm_conf) {
+        toml_free((toml_table_t*)table);
+        return 0;
+    }
+
+    if (toml_key_exists(sm_conf, "execution_policy")) {
+        current_val = toml_string_in(sm_conf, "execution_policy");
         PARSE_CONF_CHECK(current_val, "execution_policy")
         str_to_lower(current_val.u.s);
         if (strcmp(current_val.u.s, "thread_pool") == 0 || strcmp(current_val.u.s, "thread pool") == 0)
@@ -56,78 +73,79 @@ static int parse_conf(SMConfig* conf, FILE* f) {
     }
 
 
-    if (toml_key_exists(table, "n_threads")) {
-        current_val = toml_int_in(table, "n_threads");
+    if (toml_key_exists(sm_conf, "n_threads")) {
+        current_val = toml_int_in(sm_conf, "n_threads");
         PARSE_CONF_CHECK(current_val, "n_threads")
         conf->n_threads = (size_t)current_val.u.i;
     }
 
-    if (toml_key_exists(table, "init_percent_x")) {
-        current_val = toml_double_in(table, "init_percent_x");
+    if (toml_key_exists(sm_conf, "init_percent_x")) {
+        current_val = toml_double_in(sm_conf, "init_percent_x");
         PARSE_CONF_CHECK(current_val, "init_percent_x");
         conf->init_percent_x = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "init_percent_y")) {
-        current_val = toml_double_in(table, "init_percent_y");
+    if (toml_key_exists(sm_conf, "init_percent_y")) {
+        current_val = toml_double_in(sm_conf, "init_percent_y");
         PARSE_CONF_CHECK(current_val, "init_percent_y");
         conf->init_percent_y = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "ra")) {
-        current_val = toml_double_in(table, "ra");
+    if (toml_key_exists(sm_conf, "ra")) {
+        current_val = toml_double_in(sm_conf, "ra");
         PARSE_CONF_CHECK(current_val, "ra");
         conf->ra = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "ri")) {
-        current_val = toml_double_in(table, "ri");
+    if (toml_key_exists(sm_conf, "ri")) {
+        current_val = toml_double_in(sm_conf, "ri");
         PARSE_CONF_CHECK(current_val, "ri");
         conf->ri = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "b1")) {
-        current_val = toml_double_in(table, "b1");
+    if (toml_key_exists(sm_conf, "b1")) {
+        current_val = toml_double_in(sm_conf, "b1");
         PARSE_CONF_CHECK(current_val, "b1");
         conf->b1 = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "d1")) {
-        current_val = toml_double_in(table, "d1");
+    if (toml_key_exists(sm_conf, "d1")) {
+        current_val = toml_double_in(sm_conf, "d1");
         PARSE_CONF_CHECK(current_val, "d1");
         conf->d1 = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "b2")) {
-        current_val = toml_double_in(table, "b2");
+    if (toml_key_exists(sm_conf, "b2")) {
+        current_val = toml_double_in(sm_conf, "b2");
         PARSE_CONF_CHECK(current_val, "b2");
         conf->b2 = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "d2")) {
-        current_val = toml_double_in(table, "d2");
+    if (toml_key_exists(sm_conf, "d2")) {
+        current_val = toml_double_in(sm_conf, "d2");
         PARSE_CONF_CHECK(current_val, "d2");
         conf->d2 = (float)current_val.u.d;
     }
     
-    if (toml_key_exists(table, "alpha_n")) {
-        current_val = toml_double_in(table, "alpha_n");
+    if (toml_key_exists(sm_conf, "alpha_n")) {
+        current_val = toml_double_in(sm_conf, "alpha_n");
         PARSE_CONF_CHECK(current_val, "alpha_n");
         conf->alpha_n = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "alpha_m")) {
-        current_val = toml_double_in(table, "alpha_m");
+    if (toml_key_exists(sm_conf, "alpha_m")) {
+        current_val = toml_double_in(sm_conf, "alpha_m");
         PARSE_CONF_CHECK(current_val, "alpha_m");
         conf->alpha_m = (float)current_val.u.d;
     }
 
-    if (toml_key_exists(table, "dt")) {
-        current_val = toml_double_in(table, "dt");
+    if (toml_key_exists(sm_conf, "dt")) {
+        current_val = toml_double_in(sm_conf, "dt");
         PARSE_CONF_CHECK(current_val, "dt");
         conf->dt = (float)current_val.u.d;
     }
 
+    toml_free((toml_table_t*)table);
     return 0;
 }
 
@@ -167,7 +185,7 @@ int gen_config(Config* conf, int argc, const char** argv) {
         fprintf(stderr, "Cannot open the file: '%s'\n", argv[1]);      // TODO: better errors
         return -1;
     }
-    int parse_res = parse_conf(&conf->sm_conf, file);
+    int parse_res = parse_conf(conf, file);
     fclose(file);
     if (parse_res == -1)
         return -1;
